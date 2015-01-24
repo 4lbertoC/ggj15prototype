bcacddc18fd08837259758f946fbc76a607f44ca
Alberto Congiu <4lbertoC@gmail.com>
2015-01-24 16:31:08 +0100

    updated demoscene

--- Assets/Scripts/Guy.cs
+++ Assets/Scripts/Guy.cs
@@ -11,13 +11,18 @@ public class Guy : MonoBehaviour
 		private int guyId;
 		private GameState gameState = GameState.GetInstance ();
 		private List<Arm> arms = new List<Arm> ();
-	    private GameObject balloon;
-	    private int framesBeforeShuttingUp = -1;
+		private GameObject balloon;
+		private int framesBeforeShuttingUp = -1;
+		private bool isSaved = false;
+		private bool goingAwayTime = 0;
+		private bool isGone = false;
+
+		private const Vector3 hiddenScale = new Vector3 (0, 1, 0);
 
 		void Awake ()
 		{
 				guyId = guyIdCumulative++;
-				Debug.Log("Guy #" + guyId + " was awaken");
+				Debug.Log ("Guy #" + guyId + " was awaken");
 		}
 
 		// Use this for initialization
@@ -28,34 +33,48 @@ public class Guy : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
-			if (framesBeforeShuttingUp > 0) {
-				framesBeforeShuttingUp--;
-			} else if (framesBeforeShuttingUp == 0) {
-				ShutUp();
-			}
-		}
-	
-
-		private void SpawnArmsIfNecessary() {
-			if (arms.Count == 0) {
-				for (int armIndex = 0; armIndex < Game.ARMS_COUNT; armIndex++) {			
-					GameObject arm = Instantiate (armPrefab, this.GetPosition (), Quaternion.identity) as GameObject;
-					arm.transform.parent = this.transform;						
-					// arm.transform.rotation = Quaternion.LookRotation (targetGuy.GetPosition () - this.GetPosition ());
-					arms.Add (arm.GetComponent<Arm>());
-					Debug.Log ("Arm #" + armIndex + " of guy #" + GetId() + " spawned");
+				if (framesBeforeShuttingUp > 0) {
+						framesBeforeShuttingUp--;
+				} else if (framesBeforeShuttingUp == 0) {
+						ShutUp ();
 				}
-			}
+
+				if (goingAwayTime > 0) {
+			transform.localScale.Set (Vector3.Lerp(transform.localScale, hiddenScale, 1);
+			                          }
+			                                       }
+		}
+
+		private void SpawnArmsIfNecessary ()
+		{
+				if (arms.Count == 0) {
+						for (int armIndex = 0; armIndex < Game.ARMS_COUNT; armIndex++) {			
+								GameObject arm = Instantiate (armPrefab, this.GetPosition (), Quaternion.identity) as GameObject;
+								arm.transform.parent = this.transform;						
+								// arm.transform.rotation = Quaternion.LookRotation (targetGuy.GetPosition () - this.GetPosition ());
+								arms.Add (arm.GetComponent<Arm> ());
+								Debug.Log ("Arm #" + armIndex + " of guy #" + GetId () + " spawned");
+						}
+				}
+		}
+
+		private void RemoveFromScene ()
+		{
+				isSaved = true;
+				isGoingAway = true;
 		}
 		
 		void OnMouseDown ()
 		{
+				if (isSaved) {
+						return;
+				}
 				bool isPedro = gameState.isPedro (this);
 				Debug.Log ("Clicked " + guyId + ". " + (isPedro ? "Is Pedro!" : ""));
 				if (isPedro) {
 						gameState.RemoveGuy (this);
-						ShutUp();
-						Destroy (this.gameObject);		
+						ShutUp ();
+						RemoveFromScene ();
 						gameState.ResetGame ();
 						transform.parent.gameObject.BroadcastMessage ("OnGuysUpdate");
 				} else {
@@ -64,26 +83,29 @@ public class Guy : MonoBehaviour
 			
 		}
 		
-		private Arm GetArm(int armIndex) {
-			SpawnArmsIfNecessary();
-			return arms [armIndex];
+		private Arm GetArm (int armIndex)
+		{
+				SpawnArmsIfNecessary ();
+				return arms [armIndex];
 		}
 		
-		public void AimAt (int armIndex, Guy targetGuy) {
-				Debug.Log ("Arm #" + armIndex + " of guy #" + GetId() + " aiming @ " + targetGuy.GetId());
+		public void AimAt (int armIndex, Guy targetGuy)
+		{
+				Debug.Log ("Arm #" + armIndex + " of guy #" + GetId () + " aiming @ " + targetGuy.GetId ());
 				Arm arm = GetArm (armIndex);
 				if (arm == null) {
-					Debug.Log ("No arm!");
+						Debug.Log ("No arm!");
 				}
 				arm.target = targetGuy.transform;
-				arm.Show();
+				arm.Show ();
 		}
 		
-		public void AimAtNobody (int armIndex) {
-			Debug.Log ("Arm #" + armIndex + " of guy #" + GetId() + " aiming @ nobody");
-			Arm arm = GetArm (armIndex);			
-			arm.target = null;
-			// arm.Hide();
+		public void AimAtNobody (int armIndex)
+		{
+				Debug.Log ("Arm #" + armIndex + " of guy #" + GetId () + " aiming @ nobody");
+				Arm arm = GetArm (armIndex);			
+				arm.target = null;
+				// arm.Hide();
 		}
 
 		public Vector3 GetPosition ()
@@ -99,22 +121,23 @@ public class Guy : MonoBehaviour
 		public void Speak (string sentence)
 		{
 			
-			if (balloon == null) {
-				balloon = Instantiate (balloonPrefab, 
-	                this.GetPosition () + new Vector3(0.1f, 1.0f, -2.0f),
+				if (balloon == null) {
+						balloon = Instantiate (balloonPrefab, 
+	                this.GetPosition () + new Vector3 (0.1f, 1.0f, -2.0f),
                		Quaternion.identity) as GameObject;
-				// balloon.transform.parent = this.transform;
-			}
-			TextMesh sentenceTextMesh = balloon.GetComponentInChildren<TextMesh>();
-			sentenceTextMesh.text = sentence;
-			balloon.SetActive(true);
-			framesBeforeShuttingUp = 90;
+						// balloon.transform.parent = this.transform;
+				}
+				TextMesh sentenceTextMesh = balloon.GetComponentInChildren<TextMesh> ();
+				sentenceTextMesh.text = sentence;
+				balloon.SetActive (true);
+				framesBeforeShuttingUp = 90;
 		}
 		
-		public void ShutUp() {
-			if (balloon != null) {
-				balloon.SetActive (false);
-			}
-			framesBeforeShuttingUp = -1;
+		public void ShutUp ()
+		{
+				if (balloon != null) {
+						balloon.SetActive (false);
+				}
+				framesBeforeShuttingUp = -1;
 		}
 }
