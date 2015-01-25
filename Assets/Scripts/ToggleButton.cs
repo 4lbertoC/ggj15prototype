8b48ed0123ca38382a0de944349b67542cdf1482
Alberto Congiu <4lbertoC@gmail.com>
2015-01-25 14:01:45 +0100

    added credits

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleButton : MonoBehaviour {
+
+	public GameObject toggledItem;
+
+	private bool isShow = false;
+
+	void OnMouseDown() {
+		toggledItem.SetActive (!isShow);
+		isShow = !isShow;
+	}
+}
