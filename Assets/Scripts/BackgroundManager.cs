3d73248b4ebaead9d233de9585583e4ad90dd709
Mauro Vanetti <posta@maurovanetti.info>
2016-03-09 12:19:49 +0100

    Fixed background switch

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -4,29 +4,38 @@ using System.Collections.Generic;
 
 public class BackgroundManager : MonoBehaviour
 {
-		private GameState gameState = GameState.GetInstance ();
-		private float nextTransition = 0;
-		private Quaternion from;
-		private Quaternion to;
+    private float nextTransition = 0;
+    private Quaternion from;
+    private Quaternion to;
+    private int rotationDegrees = 0;
 
-		void Awake ()
-		{
-				gameState.OnNewStandoff += NextPhase;
-				from = this.gameObject.transform.rotation;
-		}
+    void Awake()
+    {
+        GameState.GetInstance().OnNewStandoff += NextPhase;
+        from = Quaternion.Euler(0, rotationDegrees, 0);
+        transform.rotation = from;
 
-		public void NextPhase ()
-		{
-				from = this.gameObject.transform.rotation;
-				to = from * Quaternion.Euler (0, 90, 0);
-				nextTransition = 0.5f;
-		}
+    }
 
-		void Update ()
-		{
-				if (nextTransition > 0) {
-						nextTransition -= Time.deltaTime;
-						transform.rotation = Quaternion.Lerp (from, to, (0.2f - nextTransition) / 0.2f);
-				}
-		}
+    public void NextPhase()
+    {
+        from = this.gameObject.transform.rotation;
+        rotationDegrees += 90;
+        rotationDegrees %= 360;
+        to = Quaternion.Euler(0, rotationDegrees, 0);
+        nextTransition = 0.5f;
+    }
+
+    void Update()
+    {
+        if (nextTransition > 0)
+        {
+            nextTransition -= Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(from, to, (0.2f - nextTransition) / 0.2f);
+        }
+        else
+        {
+            transform.rotation = to;
+        }
+    }
 }
